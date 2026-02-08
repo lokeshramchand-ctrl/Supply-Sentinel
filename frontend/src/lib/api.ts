72c24b20def2d5979d7991ac50a7909f278d86ef
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-02-08 17:48:07 +0000

    Add Dockerfile for building and running Next.js application

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000";
-
+//const BASE_URL = "http://127.0.0.1:8000"; --> local development
+const BASE_URL = "https://occo8004goko4o048808c4os.deploy.priyatham.in";
 export async function fetchHistory() {
   const res = await fetch(`${BASE_URL}/agent/history`);
   return res.json();
